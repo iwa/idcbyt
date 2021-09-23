dd4e006b203743493cf315dca3a7880a31480427
iwa <iwaongit@gmail.com>
2021-09-23 12:52:59 +0200

    feat: :sparkles: add a stats command only for iwa

---
+++ src/commands/utility/stats.ts
@@ -0,0 +1,28 @@
+import Bot from '../../Client'
+import { Message } from 'discord.js';
+import Command from '../../structures/Command';
+import PermLevels from '../../structures/PermLevels';
+
+export default new class StatsCommand extends Command {
+
+    public constructor() {
+        super('stats',
+            SendStats,
+            PermLevels.Iwa,
+            ['stat'],
+            ['EMBED_LINKS'],
+            'stats');
+    }
+
+}
+
+async function SendStats(msg: Message) {
+    let ping = Math.ceil(Bot.ws.ping);
+    let guilds = Bot.guilds.cache.size;
+    let users = Bot.users.cache.size;
+    let VCs = Bot.music.players.size;
+
+    await msg.channel.send(Bot.createEmbed('⚙️ Stats', `ping: \`${ping}ms\`\nguilds: \`${guilds}\`\nusers: \`${users}\`\nvoice: \`${VCs}\` connections`))
+        .then(() => { Bot.log.info({ msg: 'ping', author: { id: msg.author.id, name: msg.author.tag }, guild: { id: msg.guild.id, name: msg.guild.name } }); })
+        .catch(err => Bot.log.error(err));
+}
